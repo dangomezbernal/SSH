617855bcda34e0821b1be0b835c8ef9db3bb12c4
Dan Gómez <dangomezbernal@gmail.com>
2022-12-01 23:13:31 +0000

    Upload New File

---
+++ ssh22:base/startup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+for user in unix01 unix02 unix03 unix04 unix05
+do
+	useradd -m -s /bin/bash $user
+	echo -e "$user\n$user" | passwd $user
+done
+
+/usr/sbin/nslcd
+
+mkdir /run/sshd
+/usr/sbin/sshd  -D
+
